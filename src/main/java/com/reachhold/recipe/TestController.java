cb49db477a6a31b665c3192df424b19c83596429
teheidoma <novakovskii2002@gmail.com>
2024-09-26 05:28:29 +0300

    main panel

--- src/main/java/com/reachhold/recipe/TestController.java
+++ src/main/java/com/reachhold/recipe/TestController.java
@@ -8,12 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 @RestController
 public class TestController {
